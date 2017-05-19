7cbd46eb03c898baa65a70bd6e7628aa7ba76455
pontanVB <pontus.w.m@gmail.com>
2017-05-19 20:45:34 +0200

    Camera

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    public float mouseChange = 0.7f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = target.position + new Vector3(0, 0, -10) + mousePosition.normalized / mouseChange;
+
+    }
+}
