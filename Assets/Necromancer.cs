d55c62aaf83af792775bde056d8f7754d888ddac
pontanVB <pontus.w.m@gmail.com>
2017-05-19 22:43:05 +0200

    Necromancer

---
+++ Assets/Necromancer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Necromancer : MonoBehaviour
+{
+    public GameObject summoningCircle;
+    Transform target;
+    public float speed = 1;
+    float time;
+    public float attackTimer = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        Vector3 direction = target.position - transform.position;
+        transform.position -= direction.normalized * speed * Time.deltaTime;
+
+        if (time > attackTimer)
+        {
+            Instantiate(summoningCircle, target.position, target.rotation);
+            time = 0;
+        }
+
+    }
+}
---
+++ Assets/Summon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summon : MonoBehaviour
+{
+    public GameObject lightning;
+    public GameObject skeleton;
+    float time;
+    public float timer;
+    public float spawndelay;
+    bool oneAttack = false;
+    GameObject i;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (timer < time && oneAttack == false)
+        {
+            i = Instantiate(lightning, transform.position, transform.rotation);
+            oneAttack = true;
+
+        }
+
+        if (timer < time - spawndelay)
+        {
+            Instantiate(skeleton, transform.position, transform.rotation);
+            Destroy(i);
+            Destroy(gameObject);
+        }
+
+    }
+}
