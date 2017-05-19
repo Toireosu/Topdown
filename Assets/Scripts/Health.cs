05e7f259c1f75833a3866d4cfc8ceb2e50964769
BuildTools <benjaminnettervik@hotmail.com>
2017-05-19 20:45:40 +0200

    Saker som e gay
    Indeed

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float maxHealth;
+    public float currentHealth;
+    public float minHealth;
+
+
+}
