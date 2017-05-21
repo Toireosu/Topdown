8f2f9d2af176341c6ce3a151caebed795ba7e278
zweetdrmz <benjaminnettervik@hotmail.com>
2017-05-21 10:56:38 +0200

    Grenade adding

---
+++ Assets/Scripts/Grenade/Grenade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    GrenadeThrow gtThrow;
+    Vector2 grenadeDirection;
+    [HideInInspector]
+    public Rigidbody2D rb;
+
+    public float lifeTime;
+    public float speed;
+    public GameObject explosion;
+
+    private void Start()
+    {
+        SetUpGrenade(lifeTime, speed);
+        StartCoroutine(ExplodeGrenadeEnum(false, lifeTime, explosion));
+    }
+
+    public void SetUpGrenade(float lifeTime, float speed)
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gtThrow = GameObject.FindGameObjectWithTag("Player").GetComponent<GrenadeThrow>();
+        //Grenade Direction
+        grenadeDirection = Camera.main.ScreenToWorldPoint(gtThrow.mousePosOnThrow) - transform.position;
+        //Add force in direction
+        rb.AddForce(grenadeDirection * speed * Time.deltaTime);
+        //Explode after time
+    }
+
+    public void ExplodeGrenade(GameObject explosion)
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    public IEnumerator ExplodeGrenadeEnum(bool status, float delayTime, GameObject explosion)
+    {
+        yield return new WaitForSeconds(delayTime);
+        ExplodeGrenade(explosion);
+    }
+
+}
---
+++ Assets/Scripts/Grenade/NaziGrenade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaziGrenade : Grenade
+{
+    
+    //This script is for NaziGrenade (sieg heil) abilities only
+
+    public float spinAmount;
+
+    void Update()
+    {
+        //Moves the grenade towards direction
+        GrenadeMovement();
+    }
+
+    void GrenadeMovement()
+    {
+        if (rb.velocity.magnitude < 1.5)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        transform.Rotate(Vector3.forward * spinAmount * rb.velocity.magnitude * Time.deltaTime);
+    }
+}
