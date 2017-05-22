3c4f2b4528bd893c3c025fe5409464c5183fcc4e
William Lindgren <william.lindgren19@gmail.com>
2017-05-22 20:28:11 +0200

    enemy eye is now smarter

--- Assets/Scripts/Enemy_eye.cs
+++ Assets/Scripts/Enemy_eye.cs
@@ -9,26 +9,49 @@ public class Enemy_eye : MonoBehaviour
     [SerializeField] [Range(0.0f, 60.0f)] float rotateSpeed = 5; 
     [SerializeField] [Range(0.0f, 20.0f)] float range = 5;
 
+    bool rotateReverse = false;
+    public float timeKnockedOut = 2;
 
-    // Update is called once per frame
+    Rigidbody2D rb2d;
+
+    float bounce = 0;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
+        bounce -= Time.deltaTime;
         Vector3 direction = target.position - transform.position;
-
-
-        // Movement towards/away from target
-        if (direction.magnitude > range)
+        if (bounce <= 0)
         {
-            transform.position += direction.normalized * Time.deltaTime * speed;
-        }
-        else if(direction.magnitude < range - 1)
-        {
-            transform.position -= direction.normalized * Time.deltaTime * 2 * speed;
-        }
+            rb2d.velocity = new Vector2();
 
-        // Movement around target
-        transform.position += -transform.up * Time.deltaTime * rotateSpeed;
 
+            // Movement towards/away from target
+            if (direction.magnitude > range)
+            {
+                transform.position += direction.normalized * Time.deltaTime * speed;
+            }
+            else if(direction.magnitude < range - 1)
+            {
+                transform.position -= direction.normalized * Time.deltaTime * 2 * speed;
+            }
+
+            if (rotateReverse)
+            {
+                transform.position += transform.up * Time.deltaTime * rotateSpeed;
+            } 
+            else
+            {
+                // Movement around target
+                transform.position += -transform.up * Time.deltaTime * rotateSpeed;
+            }
+
+
+        }
         //Rotate towards target
         // Angle calculation
         float adjacent = target.position.x - transform.position.x;
@@ -38,5 +61,15 @@ public class Enemy_eye : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (bounce <= 0)
+        {
+            rb2d.velocity = -(collision.transform.position - transform.position).normalized * 4;
+            bounce = timeKnockedOut;
+            rotateReverse = !rotateReverse;
+        }
     }
 }
