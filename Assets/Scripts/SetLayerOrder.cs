da280c43146739a7255129d4d1dda93a6a3eafb7
zweetdrmz <benjaminnettervik@hotmail.com>
2017-05-23 20:00:56 +0200

    layer script

---
+++ Assets/Scripts/SetLayerOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLayerOrder : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    public int order;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        sr.sortingOrder = ((int)(transform.position.y * 10) - order) * -1;
+        //print(sr.sortingOrder);
+    }
+}
