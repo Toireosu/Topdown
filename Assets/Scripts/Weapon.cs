3ed7cbac24bef014c1a45834c371e50453465958
William Lindgren <william.lindgren19@gmail.com>
2017-05-23 20:01:20 +0200

    MuzzleFlash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -15,14 +15,9 @@ public class Weapon : MonoBehaviour
     [HideInInspector]
     public Transform barrelPos;
 
-    public Vector3 offset;
+    public GameObject muzzleFlash;
 
     public void Fire(PlayerAim.Direction direction)
-    {
-        Shoot(direction);
-    }
-
-    public virtual void Shoot(PlayerAim.Direction direction)
     {
         Vector3 dir = -transform.right;
         barrelPos = pos1;
@@ -31,6 +26,18 @@ public class Weapon : MonoBehaviour
             dir = transform.right;
             barrelPos = pos2;
         }
+        // Create the muzzle flash
+        GameObject f = Instantiate(muzzleFlash, barrelPos.position, transform.rotation);
+        f.transform.SetParent(barrelPos);
+        Mover moverf = f.GetComponent<Mover>();
+        moverf.lifeTime = 0.1f;
+        if (direction == PlayerAim.Direction.RIGHT) { f.GetComponent<SpriteRenderer>().flipX = true; }
+        Shoot(dir);
+    }
+
+    public virtual void Shoot(Vector3 dir)
+    {
+        // Create the bullet
         GameObject g = Instantiate(bullet, barrelPos.position, transform.rotation);
         Mover mover = g.GetComponent<Mover>();
         mover.direction = dir;
