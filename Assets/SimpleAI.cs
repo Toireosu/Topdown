d01b7bc6637a10e5e9dc342e6bc47b924488aecc
William Lindgren <william.lindgren19@gmail.com>
2017-05-23 23:21:12 +0200

    Goblins are now intelligent!

---
+++ Assets/SimpleAI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+	
+	void Update () {
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * Time.deltaTime * speed;
+	}
+}
