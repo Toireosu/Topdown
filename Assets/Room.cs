0a5d382dc083323382de29b2e092bd0102e9aaff
William Lindgren <william.lindgren19@gmail.com>
2017-05-26 20:21:49 +0200

    Light and shit

---
+++ Assets/Room.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public Light[] lights;
+	
+	void Start () {
+        foreach (Light light in lights)
+        {
+            light.enabled = false;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            foreach (Light light in lights)
+            {
+                light.enabled =true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            foreach (Light light in lights)
+            {
+                light.enabled = false;
+            }
+        }
+    }
+}
